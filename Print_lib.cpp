81f358074db3619a1000f3e88c01eea2288b8092
heath <heathhescock@gmail.com>
2020-04-23 01:11:56 -0400

    added more functionality

--- Print_lib.cpp
+++ Print_lib.cpp
@@ -1,10 +1,13 @@
 #include "Print_lib.h"
 
+//////////////////////////////////////////////////////////////////////
+
 Print_lib::Print_lib(SoftwareSerial* serial)
 {
   m_hw = false;
   m_port = serial;
 };
+
 
 Print_lib::Print_lib(HardwareSerial* serial)
 {
@@ -12,8 +15,9 @@ Print_lib::Print_lib(HardwareSerial* serial)
   m_port = serial;
 };
 
+//////////////////////////////////////////////////////////////////////
 
-void Print_lib::Begin(uint32_t baud)
+void Print_lib::Begin(const uint32_t baud)
 {
   if(m_hw)
   {
@@ -23,4 +27,98 @@ void Print_lib::Begin(uint32_t baud)
   {
     static_cast<SoftwareSerial*>(m_port)->begin(baud);
   }
+}
+
+//////////////////////////////////////////////////////////////////////
+// function doesn't work properly
+void Print_lib::Print_array(const char* string,
+                            const uint8_t array[],
+                            const uint8_t length,
+                            const bool hex)
+{
+  // TODO: if last char is not space space println
+  m_port->print(string);
+  Print_array(array, length, hex);
+}
+
+void Print_lib::Print_array(const uint8_t array[],
+                            const uint8_t length,
+                            const bool hex)
+{
+  if(hex)
+  {
+    for(uint8_t i = 0; i < length; i++)
+    {
+      if(i != 0)
+      {
+        m_port->print(", ");
+      }
+      m_port->print(array[i], HEX);
+    }
+  }
+  else
+  {
+    for(uint8_t i = 0; i < length; i++)
+    {
+      if(i != 0)
+      {
+        m_port->print(", ");
+      }
+      m_port->print(array[i]);
+    }
+  }
+  m_port->println();
+}
+
+//////////////////////////////////////////////////////////////////////
+
+void Print_lib::Print_msg(const char* string,
+                          const Msg_data msg,
+                          bool hex)
+{
+  m_port->println(string);
+  Print_msg(msg, hex);
+}
+
+void Print_lib::Print_msg(const Msg_data msg, bool hex)
+{
+  if(hex)
+  {
+    m_port->print("Length: ");
+    m_port->println(msg.length, HEX);
+    m_port->print("Valid: ");
+    m_port->println(msg.valid, HEX);
+    m_port->print("Frame type: ");
+    m_port->println(msg.frame_type, HEX);
+    m_port->print("Address: ");
+    m_port->println(msg.address, HEX);
+    m_port->print("Count: ");
+    m_port->println(msg.payload_cnt, HEX);
+    m_port->print("Payload id: ");
+    m_port->println(msg.payload_id, HEX);
+    m_port->print("Payload length: ");
+    m_port->println(msg.payload_len, HEX);
+    m_port->print("Payload: ");
+    Print_array(msg.payload, sizeof(msg.payload));
+  }
+  else
+  {
+    m_port->print("Length: ");
+    m_port->println(msg.length);
+    m_port->print("Valid: ");
+    m_port->println(msg.valid);
+    m_port->print("Frame type: ");
+    m_port->println(msg.frame_type);
+    m_port->print("Address: ");
+    m_port->println(msg.address);
+    m_port->print("Count: ");
+    m_port->println(msg.payload_cnt);
+    m_port->print("Payload id: ");
+    m_port->println(msg.payload_id);
+    m_port->print("Payload length: ");
+    m_port->println(msg.payload_len);
+    m_port->print("Payload: ");
+    Print_array(msg.payload, sizeof(msg.payload), false);
+  }
+  m_port->println();
 }
--- Print_lib.h
+++ Print_lib.h
@@ -1,7 +1,6 @@
 #ifndef Print_lib_h
 #define Print_lib_h
 
-///#include "Arduino.h"
 #include "Xbee_lib_defs.h"
 #include "SoftwareSerial.h"
 
@@ -9,27 +8,48 @@ class Print_lib
 {
 public:
   Print_lib(SoftwareSerial* ss);
+
   Print_lib(HardwareSerial* ss);
-  void Begin(uint32_t baud);
+
+  void Begin(const uint32_t baud);
 
   template <typename T>
-  void  Print(T x)
+  void  Print(const T x)
   {
     m_port->print(x);
   }
 
+  void Println()
+  {
+    m_port->println();
+  }
+
   template <typename T>
-  void  Println(T x)
+  void  Println(const T x)
   {
     m_port->println(x);
   }
 
   template <typename T, typename U>
-  void  Println(T x, U y)
+  void  Println(const T x, const U y)
   {
     m_port->print(x);
     m_port->println(y);
   }
+
+  void Print_array(const char* string,
+                   const uint8_t array[],
+                   const uint8_t length,
+                   const bool hex);
+
+  void Print_array(const uint8_t array[],
+                   const uint8_t length,
+                   const bool hex = true);
+
+
+  void Print_msg(const Msg_data msg, bool hex);
+
+  void Print_msg(const char* string, const Msg_data msg, bool hex);
 
 protected:
 
